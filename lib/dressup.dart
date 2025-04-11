86c77674d9b06df35acaa1ebae93b048ebe9907e
kattmaniak <theonlydjboss@gmail.com>
2025-04-11 02:46:34 +0200

    Adjust Dress Up game, add new assets

--- lib/dressup.dart
+++ lib/dressup.dart
@@ -1,12 +1,14 @@
 // This is a skeleton for a game. It is not a complete game and will not run.
 import 'dart:async';
 import 'dart:collection';
+import 'dart:math';
 
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
+import 'package:flame/rendering.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -84,16 +86,12 @@ class DressUpGameScreen extends StatelessWidget {
 
 class PieceData {
   final String imagePath;
-  final double width;
-  final double height;
   final ClothingType type;
   final ClothingStyle style;
   final int score;
 
   PieceData({
     required this.imagePath,
-    required this.width,
-    required this.height,
     required this.type,
     required this.style,
     required this.score,
@@ -103,41 +101,80 @@ class PieceData {
 List<PieceData> piecesData = [
   PieceData(
     imagePath: 'hat.png',
-    width: 150,
-    height: 100,
     type: ClothingType.hat,
     style: ClothingStyle.goth,
     score: 10,
   ),
   PieceData(
     imagePath: 'shirt.png',
-    width: 200,
-    height: 250,
     type: ClothingType.shirt,
-    style: ClothingStyle.punk,
+    style: ClothingStyle.goth,
     score: 20,
   ),
   PieceData(
-    imagePath: 'pants.png',
-    width: 200,
-    height: 350,
+    imagePath: 'khakis.png',
     type: ClothingType.pants,
-    style: ClothingStyle.casual,
+    style: ClothingStyle.goth,
     score: 30,
   ),
+  PieceData(
+    imagePath: 'pants.png',
+    type: ClothingType.pants,
+    style: ClothingStyle.punk,
+    score: 30,
+  ),
+  PieceData(
+    imagePath: 'beanie.png',
+    type: ClothingType.hat,
+    style: ClothingStyle.punk,
+    score: 15,
+  ),
+  PieceData(
+    imagePath: 'tiedye.png',
+    type: ClothingType.shirt,
+    style: ClothingStyle.punk,
+    score: 25,
+  ),
+  PieceData(
+    imagePath: 'funnyhat.png',
+    type: ClothingType.hat,
+    style: ClothingStyle.fine,
+    score: 5,
+  ),
+  PieceData(
+    imagePath: 'dress.png',
+    type: ClothingType.shirt,
+    style: ClothingStyle.fine,
+    score: 50,
+  ),
+  PieceData(
+    imagePath: 'stockings.png',
+    type: ClothingType.pants,
+    style: ClothingStyle.fine,
+    score: 10,
+  ),
 ];
+
+Map<ClothingStyle, List> colorMap = {
+  ClothingStyle.goth: [Colors.black, Colors.red, Colors.white, Colors.purple],
+  ClothingStyle.punk: [Colors.white, Colors.blue, Colors.red, Colors.yellow],
+  ClothingStyle.fine: [Colors.purple, Colors.yellow, Colors.blue, Colors.black],
+};
 
 Future<ClothingPiece> loadClothingPiece(String imagePath) async {
   for (var pieceData in piecesData) {
     if (pieceData.imagePath == imagePath) {
+      final sprite = Sprite(await Flame.images.load(pieceData.imagePath));
+      final color = colorMap[pieceData.style]![Random().nextInt(colorMap[pieceData.style]!.length)];
       return ClothingPiece(
-        sprite: Sprite(await Flame.images.load(pieceData.imagePath), srcSize: Vector2(pieceData.width, pieceData.height), srcPosition: Vector2.zero()),
-        width: pieceData.width,
-        height: pieceData.height,
+        sprite: sprite,
+        width: sprite.originalSize.x,
+        height: sprite.originalSize.y,
         type: pieceData.type,
         style: pieceData.style,
-        score: pieceData.score,
-      );
+        color: color,
+        score: pieceData.score + Random().nextInt(5),
+      )..anchor = Anchor.center;
     }
   }
   throw Exception('Clothing piece not found');
@@ -149,6 +186,8 @@ class DressUpGameEngine extends FlameGame {
   List<ClothingPiece> clothes = [];
 
   bool finished = false;
+  late ButtonComponent finalizeButton;
+  late ButtonComponent replayButton;
 
   HashMap<ClothingType, ClothingPiece> selectedClothes = HashMap<ClothingType, ClothingPiece>();
 
@@ -157,28 +196,65 @@ class DressUpGameEngine extends FlameGame {
   static DressUpGameEngine? instance;
 
   @override
+  Color backgroundColor() => Colors.grey;
+
+  @override
   Future<void> onLoad() async {
     instance = this;
+    add(hatHitbox);
+    add(shirtHitbox);
+    add(pantsHitbox);
+
+    final background = SpriteComponent()
+      ..sprite = Sprite(await Flame.images.load('morgue.png'), srcSize: Vector2(400, 700), srcPosition: Vector2.zero())
+      ..size = Vector2(400, 700)
+      ..anchor = Anchor.center
+      ..position = Vector2(200, 350);
+    add(background);
+
     // load assets
     body = SpriteComponent()
-      ..sprite = Sprite(await Flame.images.load('body.png'), srcSize: Vector2(400, 700), srcPosition: Vector2.zero())
-      ..size = Vector2(300, 700)
+      ..sprite = Sprite(await Flame.images.load('body.png'), srcSize: Vector2(100, 250), srcPosition: Vector2.zero())
+      ..size = Vector2(100, 250)
       ..anchor = Anchor.center
-      ..position = Vector2(200, 400);
+      ..position = Vector2(200, 350)
+      ..scale = Vector2(2, 2);
 
     add(body);
 
+    List<Vector2> positions = [
+      Vector2(75, 100),
+      Vector2(75, 300),
+      Vector2(75, 500),
+      Vector2(325, 100),
+      Vector2(325, 300),
+      Vector2(325, 500),
+    ];
+
     //todo load random pieces?
 
-    clothes.add(await loadClothingPiece('hat.png')..position = Vector2(50, 50));
+    final piecesCopy = List<PieceData>.from(piecesData);
 
-    clothes.add(await loadClothingPiece('shirt.png')..position = Vector2(200, 200));
+    for (var pos in positions) {
+      if (piecesCopy.isEmpty) break;
+      int randomIndex = Random().nextInt(piecesCopy.length);
+      PieceData pieceData = piecesCopy[randomIndex];
+      piecesCopy.removeAt(randomIndex);
 
-    clothes.add(await loadClothingPiece('pants.png')..position = Vector2(200, 500));
+      ClothingPiece clothingPiece = await loadClothingPiece(pieceData.imagePath);
+      clothingPiece.position = pos;
+      clothes.add(clothingPiece);
+    }    
+
+    // clothes.add(await loadClothingPiece('hat.png')..position = Vector2(50, 50));
+
+    // clothes.add(await loadClothingPiece('shirt.png')..position = Vector2(200, 200));
+
+    // clothes.add(await loadClothingPiece('pants.png')..position = Vector2(200, 500));
 
     clothes.forEach(add);
     
-    ButtonComponent finalizeButton = ButtonComponent(
+    finalizeButton = ButtonComponent(
       button: SpriteComponent(
         sprite: Sprite(await Flame.images.load('finalize.png'), srcSize: Vector2(200, 50), srcPosition: Vector2.zero()),
         size: Vector2(200, 50),
@@ -188,16 +264,35 @@ class DressUpGameEngine extends FlameGame {
         size: Vector2(200, 50),
       ),
       size: Vector2(200, 50),
-      position: Vector2(100, 850),
+      position: Vector2(100, 640),
       onPressed: calculateScore,
     );
     add(finalizeButton);
+
+    replayButton = ButtonComponent(
+      button: SpriteComponent(
+        sprite: Sprite(await Flame.images.load('replay.png'), srcSize: Vector2(200, 50), srcPosition: Vector2.zero()),
+        size: Vector2(200, 50),
+      ),
+      buttonDown: SpriteComponent(
+        sprite: Sprite(await Flame.images.load('replay.png'), srcSize: Vector2(200, 50), srcPosition: Vector2(0, 50)),
+        size: Vector2(200, 50),
+      ),
+      size: Vector2(200, 50),
+      position: Vector2(100, 640),
+      onPressed: playAgain,
+    );
   }
 
   void calculateScore() {
+
+    removeWhere((c) => c is ClothingPiece && !selectedClothes.containsValue(c));
+
+
     // calculate score
     score = 0;
     HashMap<ClothingStyle, int> clothingScores = HashMap<ClothingStyle, int>();
+    HashMap<Color, int> colorScores = HashMap<Color, int>();
 
     List<ClothingPiece> pieces = List.empty(growable: true);
 
@@ -206,30 +301,37 @@ class DressUpGameEngine extends FlameGame {
     if(selectedClothes.containsKey(ClothingType.pants)) pieces.add(selectedClothes[ClothingType.pants]!);
 
     for (var clothingPiece in pieces) {
+      score += clothingPiece.score;
       if(clothingScores.containsKey(clothingPiece.style)) {
-        clothingScores[clothingPiece.style] = clothingScores[clothingPiece.style]! * clothingPiece.score;
+        score += clothingPiece.score * clothingScores[clothingPiece.style]!;
+        clothingScores[clothingPiece.style] = clothingScores[clothingPiece.style]! + 1;
       } else {
-        clothingScores[clothingPiece.style] = clothingPiece.score;
+        clothingScores[clothingPiece.style] = 1;
       }
-    }
-
-    for (var style in clothingScores.keys) {
-      score += clothingScores[style]!;
+      if(colorScores.containsKey(clothingPiece.color)) {
+        score += clothingPiece.score * colorScores[clothingPiece.color]!;
+        colorScores[clothingPiece.color] = colorScores[clothingPiece.color]! + 1;
+      } else {
+        colorScores[clothingPiece.color] = 1;
+      }
     }
 
     TextComponent scoreText = TextComponent(
       text: 'Score: $score',
-      position: Vector2(200, 800),
+      position: Vector2(200, 50),
       textRenderer: TextPaint(
         style: const TextStyle(
           fontSize: 24,
-          color: Colors.white,
+          color: Colors.black,
         ),
       ),
       anchor: Anchor.center,
     );
     add(scoreText);
     finished = true;
+
+    remove(finalizeButton);
+    add(replayButton);
 
     //check score for high score and update if necessary
     SharedPreferences.getInstance().then((prefs) {
@@ -245,25 +347,101 @@ class DressUpGameEngine extends FlameGame {
   }
 
   @override
+  // ignore: unnecessary_overrides
   void update(double dt) {
     super.update(dt);
   
     // update game state
   }
+
+  RectangleComponent hatHitbox = RectangleComponent(
+    position: Vector2(200,135),
+    size: Vector2(30, 30),
+    anchor: Anchor.center,
+    paint: Paint()..color = Colors.white.withAlpha(0),
+  );
+
+  RectangleComponent shirtHitbox = RectangleComponent(
+    position: Vector2(200,300),
+    size: Vector2(30, 100),
+    anchor: Anchor.center,
+    paint: Paint()..color = Colors.white.withAlpha(0),
+  );
+
+  RectangleComponent pantsHitbox = RectangleComponent(
+    position: Vector2(200,450),
+    size: Vector2(30, 70),
+    anchor: Anchor.center,
+    paint: Paint()..color = Colors.white.withAlpha(0),
+  );
   
   void handleDragEnd(ClothingPiece clothingPiece) {
-    if (clothingPiece.position.x > 200 - clothingPiece.width*0.75) {
-      if (selectedClothes.containsKey(clothingPiece.type)) {
-        selectedClothes[clothingPiece.type]?.isSelected = false;
-      }
-      selectedClothes[clothingPiece.type] = clothingPiece;
-      clothingPiece.isSelected = true;
-    } else {
-      if (selectedClothes.containsKey(clothingPiece.type)) {
-        selectedClothes[clothingPiece.type]?.isSelected = false;
-        selectedClothes.remove(clothingPiece.type);
-      }
+
+    switch (clothingPiece.type) {
+      case ClothingType.hat:
+        if(hatHitbox.containsPoint(clothingPiece.position)) {
+          clothingPiece.isSelected = true;
+          selectedClothes[clothingPiece.type] = clothingPiece;
+          remove(clothingPiece);
+          add(clothingPiece);
+        } else {
+          clothingPiece.isSelected = false;
+          selectedClothes.remove(clothingPiece.type);
+        }
+        break;
+      case ClothingType.shirt:
+        if(shirtHitbox.containsPoint(clothingPiece.position)) {
+          clothingPiece.isSelected = true;
+          selectedClothes[clothingPiece.type] = clothingPiece;
+          remove(clothingPiece);
+          add(clothingPiece);
+        } else {
+          clothingPiece.isSelected = false;
+          selectedClothes.remove(clothingPiece.type);
+        }
+        break;
+      case ClothingType.pants:
+        if(pantsHitbox.containsPoint(clothingPiece.position)) {
+          clothingPiece.isSelected = true;
+          selectedClothes[clothingPiece.type] = clothingPiece;
+          remove(clothingPiece);
+          add(clothingPiece);
+        } else {
+          clothingPiece.isSelected = false;
+          selectedClothes.remove(clothingPiece.type);
+        }
+        break;
     }
+
+    // if (clothingPiece.position.x > 200 - clothingPiece.width*0.75) {
+    //   if (selectedClothes.containsKey(clothingPiece.type)) {
+    //     selectedClothes[clothingPiece.type]?.isSelected = false;
+    //   }
+    //   selectedClothes[clothingPiece.type] = clothingPiece;
+    //   clothingPiece.isSelected = true;
+    // } else {
+    //   if (selectedClothes.containsKey(clothingPiece.type)) {
+    //     selectedClothes[clothingPiece.type]?.isSelected = false;
+    //     selectedClothes.remove(clothingPiece.type);
+    //   }
+    // }
+  }
+
+  void playAgain() {
+    // reset game state
+    score = 0;
+    finished = false;
+    selectedClothes.clear();
+    clothes.clear();
+
+    removeWhere((c) => c is ClothingPiece);
+    remove(replayButton);
+    removeWhere((c) => c is TextComponent);
+    removeWhere((c) => c is ButtonComponent);
+    removeWhere((c) => c is RectangleComponent);
+    remove(body);
+
+    onLoad();
   }
 }
 
@@ -276,7 +454,7 @@ enum ClothingType {
 enum ClothingStyle {
   goth,
   punk,
-  casual,
+  fine,
 }
 
 class ClothingPiece extends SpriteComponent with DragCallbacks {
@@ -286,25 +464,32 @@ class ClothingPiece extends SpriteComponent with DragCallbacks {
     required this.height,
     required this.type,
     required this.style,
+    required this.color,
     required this.score,
   }) : super(
     sprite: sprite,
-    size: Vector2(width, height),
+    size: Vector2(width, height)*2,
   );
 
   @override
   FutureOr<void> onLoad() {
     opacity = 0.5;
+
+    //decorator.addLast(PaintDecorator.grayscale());
+    decorator.addLast(PaintDecorator.tint(color.withAlpha(170)));
+
     return super.onLoad();
-    //TODO add colors, scoring by colors
-    // using flame decorators grayscale and then tint
   }
 
+  @override
   final Sprite sprite;
+  @override
   final double width;
+  @override
   final double height;
   final ClothingType type;
   final ClothingStyle style;
+  final Color color;
   final int score;
   
   bool draggable = true;
@@ -324,7 +509,6 @@ class ClothingPiece extends SpriteComponent with DragCallbacks {
       opacity = 0.5;
     }
   }
-
   
   @override
   void onDragStart(DragStartEvent event) {
@@ -339,15 +523,15 @@ class ClothingPiece extends SpriteComponent with DragCallbacks {
     super.onDragUpdate(event);
     // handle drag update
     Vector2 newPosition = position + event.localDelta;
-    if(newPosition.x < 0) {
-      newPosition.x = 0;
+    if(newPosition.x - width < 0) {
+      newPosition.x = width;
     } else if(newPosition.x + width > 400) {
       newPosition.x = 400 - width;
     }
-    if(newPosition.y < 0) {
-      newPosition.y = 0;
-    } else if(newPosition.y + height > 900) {
-      newPosition.y = 900 - height;
+    if(newPosition.y - height < 0) {
+      newPosition.y = height;
+    } else if(newPosition.y + height > 700) {
+      newPosition.y = 700 - height;
     }
     position = newPosition;
   }
